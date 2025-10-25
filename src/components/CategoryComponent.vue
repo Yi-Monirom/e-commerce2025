6b26a697a0da76cf0f41deeda39c14b4ab4300c6
MoniRom YI <moniromyi@gmail>
2025-10-25 20:11:10 +0700

    Ex3

---
+++ src/components/CategoryComponent.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+const componentName = 'CategoryComponent'
+</script>
+
+<template>
+  <div>this Category Component</div>
+</template>
+
+<style scoped>
+/* Add your styles here */
+</style>
